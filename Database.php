07b068f145746dc73a1bb3ac50dbdcc8aa4ad1bc
mrSemyon <syoma4k@gmail.com>
2022-06-19 23:54:40 +0500

    Realized singleton

---
+++ Database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database
+{
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct()
+    {
+        try {
+            $this->pdo = new PDO('mysql:host=localhost;dbname=test', 'root', 'root');
+            echo 'ok';
+        } catch(PDOException $exception) {
+            die($exception->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/Database.php';
+
+Database::getInstance();
+Database::getInstance();
