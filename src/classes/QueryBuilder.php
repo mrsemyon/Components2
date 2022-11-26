1ce660c326b329a45a994148b2ba20e3b698a684
mrSemyon <syoma4k@gmail.com>
2022-11-26 14:11:36 +0500

    Added `create` method to QueryBuilder class

--- src/classes/QueryBuilder.php
+++ src/classes/QueryBuilder.php
@@ -9,9 +9,18 @@ class QueryBuilder
         $this->pdo = $pdo;
     }
 
-    function getAll()
+    public function getAll()
     {
-        $statement = $this->pdo->query('SELECT * FROM `posts`');
+        $statement = $this->pdo->query("SELECT * FROM `posts`");
         return $statement->fetchAll();
+    }
+
+    public function create($table, $data)
+    {
+        $keys = implode(', ', array_keys($data));
+        $values = ':' . implode(', :', array_keys($data));
+        $statement = $this->pdo->prepare("INSERT INTO {$table} ({$keys}) VALUES ({$values})");
+        $statement->execute($data);
+        return $this->pdo->lastInsertId();
     }
 }
