32c0e1d28e69be6b350fcb1beefeb8782a25930d
mrSemyon <syoma4k@gmail.com>
2022-11-26 21:49:26 +0500

    Added controller for delete post functionality

---
+++ delete.php
@@ -0,0 +1,6 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$db->delete('posts', $_GET['id']);
+
+header('Location:index.php');
--- index.view.php
+++ index.view.php
@@ -47,7 +47,7 @@
                                     <td><a href="show.php?id=<?= $post['id'] ?>" class="text-reset text-decoration-none"><?= $post['title'] ?></a></td>
                                     <td>
                                         <a href="edit.php?id=<?= $post['id'] ?>" class="btn btn-warning">Edit</a>
-                                        <a href="delete.php" class="btn btn-danger">Delete</a>
+                                        <a href="delete.php?id=<?= $post['id'] ?>" class="btn btn-danger">Delete</a>
                                     </td>
                                 </tr>
                             <?php endforeach ?>
