0212a5699044694d709ebef68f055cbb8bebec19
mrSemyon <syoma4k@gmail.com>
2022-12-14 14:14:03 +0500

    Added aura/sqlquery via composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '32c0e1d28e69be6b350fcb1beefeb8782a25930d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '32c0e1d28e69be6b350fcb1beefeb8782a25930d',
+            'dev_requirement' => false,
+        ),
+        'aura/sqlquery' => array(
+            'pretty_version' => '2.8.0',
+            'version' => '2.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../aura/sqlquery',
+            'aliases' => array(),
+            'reference' => '3cadc8bbdeb6cde5fde01349bbd5c4b0fbc1287e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
