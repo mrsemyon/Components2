3778e70758535ad8ee68170202cd92ae9e09284c
mrSemyon <syoma4k@gmail.com>
2022-12-18 23:40:47 +0500

    Added Config class

---
+++ App/Classes/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Classes;
+
+class Config
+{
+    public static function get($path = null)
+    {
+        if ($path) {
+            $config = $GLOBALS['config'];
+            $path = explode('.', $path);
+            foreach ($path as $item) {
+                if (isset($config[$item])) {
+                    $config = $config[$item];
+                }
+            }
+            return $config;
+        }
+        return false;
+    }
+}
