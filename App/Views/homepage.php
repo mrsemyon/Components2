7432b47808bac8e792fd36f332374290f58c8a59
mrSemyon <syoma4k@gmail.com>
2022-12-18 23:43:50 +0500

    Added html table to homepage view

--- App/Views/homepage.php
+++ App/Views/homepage.php
@@ -1,4 +1,30 @@
 <?php $this->layout('layout', ['title' => 'User Profile']) ?>
 
-<h1>Home</h1>
-<p>Hello, <?=$this->e($name)?></p>
+<h1>Posts</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 mt-5">
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($posts as $post) : ?>
+            <tr>
+                <th><?= $post['id'] ?></th>
+                <td><?= $post['title'] ?></td>
+                <td>
+                    <a href="edit.php" class="btn btn-warning">Edit</a>
+                    <a href="delete.php" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+</div>
+    </div>
+</div>
